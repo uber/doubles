ab332a504bade368e217d18100a7d27e58852b6e
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-06-05 18:52:36 -0700

    Remove tests. Gonna start over much higher level.

--- test/targets/expectation_target_test.py
+++
@@ -1,6 +0,0 @@
-from doubles.targets.expectation_target import expect, ExpectationTarget
-
-
-class TestExpect(object):
-    def test_returns_an_expectation_target(self):
-        assert isinstance(expect('foo'), ExpectationTarget)
