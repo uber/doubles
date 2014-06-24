a3ef2423360a404ed57902d740921cbb6bf6eaae
Todd Sifleet <toddsifleet@Todds-MacBook-Pro.local>
2014-06-24 13:51:02 -0700

    Initial Verifying Doubles Features
    * ADd test cases for non-exsitent methods
    * Verify that methods exists on non pure doubles
    * Raise exception if the method does not exist on the underlyting object

--- doubles/proxy.py
+++ doubles/proxy.py
@@ -1,10 +1,13 @@
 from doubles.method_double import MethodDouble
+from doubles import Double
+from doubles.exceptions import VerifyingDoubleError
 
 
 class Proxy(object):
     def __init__(self, obj):
         self._obj = obj
         self._method_doubles = {}
+        self._pure_double = isinstance(self._obj, Double)
 
     def add_allowance(self, method_name):
         return self.method_double_for(method_name).add_allowance()
@@ -23,6 +26,16 @@ class Proxy(object):
     def method_double_for(self, method_name):
         if method_name in self._method_doubles:
             return self._method_doubles[method_name]
-        else:
+        elif self._pure_double or self._is_valid_method(method_name):
             method_double = self._method_doubles[method_name] = MethodDouble(method_name, self._obj)
             return method_double
+        else:
+            raise VerifyingDoubleError
+
+    def _is_valid_method(self, method_name):
+        if hasattr(self._obj, method_name):
+            method = getattr(self._obj, method_name)
+            # this way would fail on built-in methods e.g. __new__
+            # return ismethod(method) or isinstance(method, classmethod):
+            return hasattr(method, '__call__')
+        return False
