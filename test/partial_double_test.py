2917e089734ace4fd212ef9a16e8adf71d671312
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-06-24 01:00:09 -0700

    Test that only stubbed methods are altered on partial doubles.

--- test/partial_double_test.py
+++ test/partial_double_test.py
@@ -2,8 +2,13 @@ from doubles import allow, teardown
 
 
 class User(object):
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self._age = age
+
+    @property
+    def age(self):
+        return self._age
 
     def get_name(self):
         return self.name
@@ -11,16 +16,23 @@ class User(object):
 
 class TestPartialDouble(object):
     def test_stubs_real_object(self):
-        user = User('Alice')
+        user = User('Alice', 25)
 
         allow(user).to_receive('get_name').and_return('Bob')
 
         assert user.get_name() == 'Bob'
 
     def test_restores_original(self):
-        user = User('Alice')
+        user = User('Alice', 25)
         allow(user).to_receive('get_name').and_return('Bob')
 
         teardown()
 
         assert user.get_name() == 'Alice'
+
+    def test_only_affects_stubbed_method(self):
+        user = User('Alice', 25)
+
+        allow(user).to_receive('get_name').and_return('Bob')
+
+        assert user.age == 25
