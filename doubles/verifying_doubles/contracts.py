85d87cd9f309c28b5b8ac8340e3dc8de53946303
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-06-25 14:03:38 -0700

    Move verification functions to their own module.

---
+++ doubles/verifying_doubles/contracts.py
@@ -0,0 +1,16 @@
+from doubles.exceptions import VerifyingDoubleError
+
+
+def verify_method(target, method_name):
+    if not hasattr(target, method_name):
+        raise VerifyingDoubleError
+
+    attr = getattr(target, method_name)
+
+    if not callable(attr):
+        raise VerifyingDoubleError
+
+
+def verify_arguments(argspec, args, kwargs):
+    if len(argspec.args) != args:
+        raise VerifyingDoubleError
--- doubles/verifying_doubles/object_double.py
+++ doubles/verifying_doubles/object_double.py
@@ -1,21 +1,16 @@
 from inspect import getargspec
 
-from doubles.exceptions import VerifyingDoubleError
+from doubles.verifying_doubles.contracts import verify_arguments, verify_method
 from doubles.verifying_doubles.verifying_double import VerifyingDouble
 
 
 class ObjectDouble(VerifyingDouble):
     def _doubles_verify_method_name(self, method_name):
-        if not hasattr(self._doubles_target, method_name):
-            raise VerifyingDoubleError
-
-        attr = getattr(self._doubles_target, method_name)
-
-        if not callable(attr):
-            raise VerifyingDoubleError
+        verify_method(self._doubles_target, method_name)
 
     def _doubles_verify_arguments(self, method_name, args, kwargs):
-        argspec = getargspec(getattr(self._doubles_target, method_name))
-
-        if len(args) != len(argspec.args):
-            raise VerifyingDoubleError
+        verify_arguments(
+            getargspec(getattr(self._doubles_target, method_name)),
+            args,
+            kwargs
+        )
