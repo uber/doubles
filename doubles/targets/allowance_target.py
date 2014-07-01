40cbe3c1b7f3ac729f4fa126551256b0578ec640
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-06-30 17:08:32 -0700

    Use __getattribute__ to allow stubbing methods that exist on the AllowanceTarget.

--- doubles/targets/allowance_target.py
+++ doubles/targets/allowance_target.py
@@ -9,5 +9,10 @@ class AllowanceTarget(object):
     def __init__(self, target):
         self._proxy = current_space().proxy_for(target)
 
-    def __getattr__(self, method_name):
-        return self._proxy.add_allowance(method_name)
+    def __getattribute__(self, attr_name):
+        __dict__ = object.__getattribute__(self, '__dict__')
+
+        if __dict__ and attr_name in __dict__:
+            return __dict__[attr_name]
+
+        return self._proxy.add_allowance(attr_name)
--- doubles/targets/expectation_target.py
+++ doubles/targets/expectation_target.py
@@ -9,5 +9,10 @@ class ExpectationTarget(object):
     def __init__(self, target):
         self._proxy = current_space().proxy_for(target)
 
-    def __getattr__(self, method_name):
-        return self._proxy.add_expectation(method_name)
+    def __getattribute__(self, attr_name):
+        __dict__ = object.__getattribute__(self, '__dict__')
+
+        if __dict__ and attr_name in __dict__:
+            return __dict__[attr_name]
+
+        return self._proxy.add_expectation(attr_name)
