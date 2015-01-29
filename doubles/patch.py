084a44ef8ff673f4471872212e61fabbf5deeb7d
Todd Sifleet <todd.sifleet@gmail.com>
2015-01-28 21:28:29 -0800

    add docstrings to patch

--- doubles/patch.py
+++ doubles/patch.py
@@ -3,25 +3,45 @@ from doubles.exceptions import VerifyingDoubleError
 
 
 class Patch(object):
+    """
+    A wrapper around an object that has been ``patched``
+    """
     def __init__(self, target):
+        """
+        :param str path: The absolute module path to the class.
+        """
         module_path, self._name = get_path_components(target)
         self.target = get_module(module_path, target)
         self._capture_original_object()
         self.set_value(None)
 
     def _capture_original_object(self):
+        """
+        Capture the original pythong object.
+        """
         try:
             self._doubles_target = getattr(self.target, self._name)
         except AttributeError:
             raise VerifyingDoubleError(self.target, self._name)
 
     def set_value(self, value):
+        """
+        Set the value of the target.
+
+        :param obj value: The value to set.
+        """
         self._value = value
         setattr(self.target, self._name, value)
 
     @property
     def value(self):
+        """
+        Get the current value of the patch.
+        """
         return self._value
 
     def restore_original_object(self):
+        """
+        Restore the target to it's original value.
+        """
         self.set_value(self._doubles_target)
