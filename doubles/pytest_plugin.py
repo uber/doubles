b67baab2bd60d597275ce39c6e91bdab4a55d464
Stanislav Kudriashev <stas.kudriashev@gmail.com>
2018-07-22 21:50:13 -0700

    Fix doubles teardown (#135)

--- doubles/pytest_plugin.py
+++ doubles/pytest_plugin.py
@@ -4,7 +4,7 @@ from doubles.lifecycle import teardown, verify
 
 
 @pytest.hookimpl(hookwrapper=True)
-def pytest_pyfunc_call(pyfuncitem):
+def pytest_runtest_call(item):
     outcome = yield
 
     try:
